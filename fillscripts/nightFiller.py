62683120f54eadf7f814f64d194df3fa5aa339f9
Pablo <73782244+pablogiaccaglia@users.noreply.github.com>
2022-03-03 20:45:33 +0100

    improved selenium script

--- fillscripts/nightFiller.py
+++ fillscripts/nightFiller.py
@@ -93,7 +93,7 @@ class KairosBot:
 
         # collegamento con Chromium attraverso Selenium
         chrome_options = webdriver.ChromeOptions()
-        chrome_options._binary_location = os.environ.get("GOOGLE_CHROME_BINARY")
+        chrome_options._binary_location = os.environ.get("GOOGLE_CHROME_BIN")
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-dev-shm-usage")
