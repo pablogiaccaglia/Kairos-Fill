e39c655a05dbab6a547c325f7ea3470a116bcce2
Pablo <73782244+pablogiaccaglia@users.noreply.github.com>
2022-03-03 20:31:48 +0100

    improved selenium script

--- fillscripts/nightFiller.py
+++ fillscripts/nightFiller.py
@@ -93,10 +93,10 @@ class KairosBot:
 
         # collegamento con Chromium attraverso Selenium
         chrome_options = webdriver.ChromeOptions()
+        chrome_options._binary_location = os.environ.get("GOOGLE_CHROME_BINARY")
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument("--disable-dev-shm-usage")
         return chrome_options
 
     def __goToBookingPage(self, user_id, user_pw):
